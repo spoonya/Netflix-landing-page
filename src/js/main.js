ebb6b17b1d5acbb71b5e6fe5546209ac89403b34
nomad8t <serega18t@tut.by>
2020-09-10 18:00:55 +0300

    feat: adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -5,19 +5,19 @@
 //     console.log(`document ready`)
 // })
 
-document.querySelectorAll(`.faq__item-question`).forEach((item) =>
-    item.addEventListener(`click`, () => {
-        const parent = item.parentNode
+document.querySelectorAll('.faq__item-question').forEach((item) =>
+    item.addEventListener('click', () => {
+        const parent = item.parentNode;
 
-        if (parent.classList.contains(`faq__item--active`)) {
-            parent.classList.remove(`faq__item--active`)
+        if (parent.classList.contains('faq__item--active')) {
+            parent.classList.remove('faq__item--active');
         } else {
             document
-                .querySelectorAll(`.faq__item`)
-                .forEach((child) => child.classList.remove(`faq__item--active`))
+                .querySelectorAll('.faq__item')
+                .forEach((child) => child.classList.remove('faq__item--active'));
 
-            parent.classList.add(`faq__item--active`)
+            parent.classList.add('faq__item--active');
         }
     })
-)
+);
 
